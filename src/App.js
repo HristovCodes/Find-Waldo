246590a02596a42d156cd2283a7bf68ce7835746
HristovCodes <ivailohristov1990@gmail.com>
2020-08-12 07:57:35 +0300

    Moved Waldo click logic and statestate  up to app.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import "./css/reset.css";
 import React from "react";
 import WaldoImage from "./components/WaldoImage";
+import Highscores from "./components/Highscores";
+import Firebase from "./Firebase";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.startGame = this.startGame.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      game: false,
+      found: false,
+      start: 0,
+      time: Number.MAX_SAFE_INTEGER,
+      username: "",
+    };
+  }
+
+  startGame() {
+    this.setState({ game: true, start: Date.now() });
+  }
+
+  handleClick() {
+    const diff = Date.now() - this.state.start;
+    if (diff < this.state.time) {
+      Firebase.updateHighScore(diff, this.state.username);
+    }
+    this.setState({ found: true, time: diff, game: false });
+  }
+
+  handleChange(e) {
+    this.setState({ username: e.target.value });
   }
 
   render() {
-    return (
+    return this.state.game === true ? (
       <div className="App">
-        <WaldoImage img="0"></WaldoImage>
+        <WaldoImage img="0" handleClick={this.handleClick}></WaldoImage>
+      </div>
+    ) : (
+      <div className="App">
+        <button onClick={this.startGame}>START</button>
+        <label htmlFor="name">Username: </label>
+        <input type="text" onChange={this.handleChange} name="name"></input>
+        <Highscores></Highscores>
       </div>
     );
   }
--- src/components/WaldoImage.js
+++ src/components/WaldoImage.js
@@ -6,13 +6,20 @@ export default class WaldoImage extends React.Component {
     super(props);
     this._isMounted = false;
     this.getData = this.getData.bind(this);
-    this.handleClick = this.handleClick.bind(this);
     this.state = {
       link:
         "https://static.techspot.com/images2/news/bigimage/2018/08/2018-08-13-image-14.jpg",
       coords: { x: 1267, y: 86 },
-      found: false,
     };
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+    this._isMounted && this.getData(this.props.img);
   }
 
   async getData(num) {
@@ -34,24 +41,8 @@ export default class WaldoImage extends React.Component {
     this._isMounted &&
       this.setState({
         link: data.links[num] ? data.links[num] : data.links[0],
-      });
-    this._isMounted &&
-      this.setState({
         coords: data.coords[num] ? data.coords[num] : data.coords[0],
       });
-  }
-
-  handleClick() {
-    this.setState({ found: true });
-  }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-    this._isMounted && this.getData(this.props.img);
   }
 
   render() {
@@ -59,15 +50,15 @@ export default class WaldoImage extends React.Component {
       position: "absolute",
       top: this.state.coords.y,
       left: this.state.coords.x,
-      width: "20px",
-      height: "20px",
+      width: "25px",
+      height: "25px",
     };
     return (
       <div className="waldoimage">
         <img id="image" src={this.state.link} alt="waldo"></img>
         <button
           style={calcStyle}
-          onClick={this.handleClick}
+          onClick={this.props.handleClick}
           id="target"
         ></button>
       </div>
